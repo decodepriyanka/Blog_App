f34db21ce20b0a08e1f8b6ce12679c47b71909b2
Priyanka Kumari <priyankayadav7282@gmail.com>
2023-07-10 23:40:32 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import postRoutes from "./routes/posts.js";
+import userRouter from "./routes/user.js";
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+
+app.use("/posts", postRoutes);
+app.use("/user", userRouter);
+
+const CONNECTION_URL = process.env.MONGO_URI;
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(
+    "mongodb+srv://bholutiwari121:W2tYym4mJ9MZT2ap@cluster0.khelsce.mongodb.net/retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set("useFindAndModify", false);
+
+// https://ide.codingblocks.com/s/686914
